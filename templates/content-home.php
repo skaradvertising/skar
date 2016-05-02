b5806a5fe9861b8484a8550e76e65bc163d19715
Wes Piper <wpiper@skar.com>
2016-05-02 16:55:14 -0500

    home page setup

---
+++ templates/content-home.php
@@ -0,0 +1,59 @@
+<div class="scrolling-home-container">
+
+	<section class="intro home-section one">
+
+		<div class="intro-content mask">
+
+			<div class="skar-home-bg">
+				<div class="row bg-row-00"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/00.png"></div>
+				<div class="row bg-row-01"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/01.png"></div>
+				<div class="row bg-row-02"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/02.png"></div>
+				<div class="row bg-row-03"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/03.png"></div>
+				<div class="row bg-row-04"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/04.png"></div>
+				<div class="row bg-row-05"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/05.png"></div>
+				<div class="row bg-row-06"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/06.png"></div>
+				<div class="row bg-row-07"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/07.png"></div>
+				<div class="row bg-row-08"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/08.png"></div>
+				<div class="row bg-row-09"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/09.png"></div>
+				<div class="row bg-row-10"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/10.png"></div>
+				<div class="row bg-row-11"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/11.png"></div>
+				<div class="row bg-row-12"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/12.png"></div>
+				<div class="row bg-row-13"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/13.png"></div>
+				<div class="row bg-row-14"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/14.png"></div>
+				<div class="row bg-row-15"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/15.png"></div>
+				<div class="row bg-row-16"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/16.png"></div>
+				<div class="row bg-row-17"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/17.png"></div>
+				<div class="row bg-row-17"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/17.png"></div>
+				<div class="row bg-row-16"><img class="animated slideInLeft" src="/wp-content/uploads/2016/05/16.png"></div>
+				<div class="row bg-row-15"><img class="animated slideInRight" src="/wp-content/uploads/2016/05/15.png"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 big-skar-container animated zoomInRight">
+					<span class="big-skar"><?php get_template_part( 'templates/svgs/big', 'skar' ); ?></span>
+				</div>
+			</div>
+
+			<div class="row action-container">
+				<div class="action-wrapper">
+					<p>We're not like other ad agencies.</p>
+					<p>Find out why.</p>
+					<p>
+						<a class="scroll-down" href="#0"><?php get_template_part( 'templates/svgs/pxd', 'arrow' ); ?></a>
+					</p>
+				</div>
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="home-section two">
+
+	</section>
+
+	<section class="home-section three">
+
+	</section>
+
+</div>
