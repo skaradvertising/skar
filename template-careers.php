59f96376e0f7be0dfee20931e49cd2247f65af8e
Wes Piper <wpiper@skar.com>
2016-05-19 11:39:47 -0500

    Careers page finished. Also created a template file for career post type

--- template-careers.php
+++ template-careers.php
@@ -5,18 +5,35 @@
 ?>
 <div class="container-fluid careers-container">
 	<section class="row row-eq-height careers-section">
-		<div class="col-lg-5 col-md-5 col-sm-5"></div>
-		<div class="col-lg-7 col-md-7 col-sm-7 careers-content-wrapper">
-			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-5 careers-label">
-				<?php the_title(); ?>
+		<div class="col-lg-4 col-md-4 col-sm-5"></div>
+		<div class="col-lg-8 col-md-8 col-sm-7 careers-content-wrapper">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 careers-label">
+				<?php the_title('<h3>', '</h3>'); ?>
 			</div>
-			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-7 careers-content">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 careers-content">
 				<?php the_field( 'main_copy' ); ?>
 
 				<?php get_template_part( 'templates/content', 'careers' ); ?>
 
 				<?php the_field( 'bottom_copy' ); ?>
 			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"></div>
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 careers-contact">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 skar-map"><?php the_field( 'map' ); ?></div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 address"><?php the_field( 'address' ); ?></div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 social">
+				<?php if(have_rows( 'social' )) : ?>
+					<ul>
+						<?php while( have_rows( 'social' ) ) : the_row(); ?>
+							<?php if( get_sub_field( 'icon' ) ) : ?>
+								<li><a href="<?php the_sub_field( 'url' ); ?>" data-toggle="tooltip" data-placement="top" title="<?php the_sub_field( 'social_network' ); ?>" target="_blank"><img src="<?php echo get_sub_field( 'icon' )['url']; ?>"></a></li>
+							<?php endif; ?>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+				</div>
+			</div>
+
 		</div>
 	</section>
 </div>
--- templates/content-careers.php
+++ templates/content-careers.php
@@ -12,8 +12,36 @@
 <div class="career-posts">
 <?php while( $query->have_posts() ) : $query->the_post(); ?>
 	<div class="career-single-post">
-		<?php the_title(); ?>
-	</div>
+		<div class="row career-title">
+		<?php if(get_field('icon_image')) : $src = get_field('icon_image')[0]; else : $src = '/wp-content/uploads/2016/05/green-square.jpg'; endif; ?>
+			<div class="list-img">
+				<img class="img-responsive" src="<?php echo $src; ?>" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $post->ID; ?>" aria-expanded="true" aria-controls="collapse<?php echo $post->ID; ?>">
+			</div>
+			<div class="list-title">
+				<h4>
+					 <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $post->ID; ?>" aria-expanded="true" aria-controls="collapse<?php echo $post->ID; ?>">
+						<?php the_title(); ?>
+					</a>	
+				</h4>
+			</div>
+		</div>
+		<div id="collapse<?php echo $post->ID; ?>" class="collapse job-content" role="tabpanel">
+			<div class="job-description"><?php the_field( 'job_description' ); ?></div>
+			<?php if( have_rows( 'job_details' ) ) : while( have_rows( 'job_details' ) ) : the_row(); ?>
+				<div class="job-details-label"><?php the_sub_field( 'label' ); ?></div>
+				<div class="job-details"><?php the_sub_field('detail_content'); ?></div>
+			<?php endwhile; endif; ?>
+				<div class="job-form">
+				<?php if( get_field( 'apply_form' ) ) : ?>
+					<?php the_field( 'apply_form' ); ?>
+				<?php else : ?>
+					<?php echo do_shortcode( '[contact-form-7 id="12046" title="General Job App"]' ); ?>
+				<?php endif; ?>
+				</div>
+				<div class="additional-info"><?php the_field( 'additional_info' ); ?></div>
+		</div>
+
+	</div><hr>
 <?php endwhile; wp_reset_postdata(); ?>
 </div>
 <?php endif; ?>
